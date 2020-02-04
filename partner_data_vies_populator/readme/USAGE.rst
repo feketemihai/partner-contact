d4e1b64c8af52d2050f9d0d549df1861de932f31
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-02-04 16:31:10 +0200

    [MIG] partner_create_by_vat: Migration to 13.0

---
+++ partner_data_vies_populator/readme/USAGE.rst
@@ -0,0 +1,1 @@
+When changing a company partner VAT number, this module will try to fetch the partner data from VIES webservice, if available on VIES will udpate the name, address and country.
--- partner_data_vies_populator/tests/__init__.py
+++ partner_data_vies_populator/tests/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
 # ©  2015 Forest and Biomass Services Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import test_partner_data_vies_populator
